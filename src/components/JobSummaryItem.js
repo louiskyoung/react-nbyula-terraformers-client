d085ee3d9ad39b783500729d819886cdf3d0da8e
Louis Young <cool.young0420@gmail.com>
2022-09-20 12:47:20 +0800

    Update style of JobSummaryItem

--- src/components/JobSummaryItem.js
+++ src/components/JobSummaryItem.js
@@ -64,7 +64,7 @@ function JobSummaryItem({ data, setModalData, rearrangeJob }) {
           {data.title}
         </h5>
         <div className="text-sm flex flex-row items-center text-gray-700 dark:text-gray-400">
-          <span className="mr-5">
+          <span className="mr-3">
             <svg
               className="w-4 h-4 mr-1 inline"
               fill="none"
@@ -79,9 +79,9 @@ function JobSummaryItem({ data, setModalData, rearrangeJob }) {
                 d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
               ></path>
             </svg>
-            Applicants: {data.applicants.length}
+            {data.applicants.length}
           </span>
-          <span>
+          <span className="mr-3">
             <svg
               className="w-4 h-4 mx-1 inline"
               fill="none"
@@ -96,14 +96,31 @@ function JobSummaryItem({ data, setModalData, rearrangeJob }) {
                 d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
               ></path>
             </svg>
-            Deadline: {DateTime.fromISO(data.deadline).toFormat('yyyy LLL dd')}
+            {DateTime.fromISO(data.deadline).toFormat('yyyy-MM-dd')}
+          </span>
+          <span>
+            <svg
+              className="w-4 h-4 mx-1 inline"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              ></path>
+            </svg>
+            {data.location}
           </span>
         </div>
         <p className="font-normal text-gray-800 dark:text-gray-300">
           {`${data.description.slice(0, 100)}...`}
         </p>
         <div className="flex flex-row justify-end">
-          <Button size="sm" onClick={handleReadMore}>
+          <Button size="xs" color="light" onClick={handleReadMore}>
             Read more
             <svg
               className="ml-2 -mr-1 h-4 w-4"
